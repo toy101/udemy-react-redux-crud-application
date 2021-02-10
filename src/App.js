7427dde4cf7ec325f2093a69fc1adaf74400460f
toy101 <toy101dev@gmail.com>
2021-02-11 02:43:18 +0900

    learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>
-          Hello, world!
-    </h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor='bar'>bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
