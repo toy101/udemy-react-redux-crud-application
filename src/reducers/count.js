0e43074731ad43670b627b1ecf3670018196da7c
toy101 <toy101dev@gmail.com>
2021-02-15 20:10:18 +0900

    reducerの実装

---
+++ src/reducers/count.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { INCREMENT, DECREMENT } from "../actions";
+
+const initialState = { value : 0 }
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return { value : state.value + 1 }
+        case DECREMENT:
+            return { value : state.value - 1 }
+        default:
+            return state
+    }
+}
